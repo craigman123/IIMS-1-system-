f012bbaf5d5f0d55b7d9ef56d214f785a9758715
craigman123 <craigfrenan0@gmail.com>
2025-09-10 15:44:37 +0800

    dont touch

---
+++ src/Config/conf.java
@@ -0,0 +1,19 @@
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class conf {
+    
+    public static Connection connectDB() {
+        Connection con = null;
+        try {
+            Class.forName("org.sqlite.JDBC"); // Load the SQLite JDBC driver
+            con = DriverManager.getConnection("jdbc:sqlite:Main.db"); // Establish connection
+            System.out.println("Connection Successful");
+        } catch (Exception e) {
+            System.out.println("Connection Failed: " + e);
+        }
+        return con;
+    }
+}
